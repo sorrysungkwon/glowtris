2ba0b4c306c71ed98398d7e710a8b42f59ed1ec3
sorrysungkwon <seonqwer@gmail.com>
2026-06-05 09:57:57 +0000

    fix: remove onclick from perf-indicator to prevent ghost touches quitting the game

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE      = 'glowtris-2de3d2';
+const CACHE      = 'glowtris-d17073';
 const FONT_CACHE = 'glowtris-fonts-v1';
 const APP_SHELL  = ['/index.html', '/manifest.json', '/favicon.svg', '/icon-192.svg', '/icon-512.svg', '/icon-192.png', '/icon-512.png', '/apple-touch-icon.png'];
 const FONT_URLS  = [
