37fcee2553ade8acaf5403bb664550979aff152b
sorrysungkwon <seonqwer@gmail.com>
2026-06-05 10:02:29 +0000

    design: move offline and update banners to bottom to avoid dynamic island/live activity overlap

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE      = 'glowtris-d17073';
+const CACHE      = 'glowtris-da0f43';
 const FONT_CACHE = 'glowtris-fonts-v1';
 const APP_SHELL  = ['/index.html', '/manifest.json', '/favicon.svg', '/icon-192.svg', '/icon-512.svg', '/icon-192.png', '/icon-512.png', '/apple-touch-icon.png'];
 const FONT_URLS  = [
