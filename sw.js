a14b45ed2d94458da0c91039e25edf3dac69c591
sorrysungkwon <seonqwer@gmail.com>
2026-06-07 06:33:27 +0000

    feat: restructure footer to place Blog on its own line

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE      = 'glowtris-1c4937';
+const CACHE      = 'glowtris-cefd35';
 const FONT_CACHE = 'glowtris-fonts-v1';
 const APP_SHELL  = ['/index.html', '/manifest.json', '/favicon.svg', '/icon-192.svg', '/icon-512.svg', '/icon-192.png', '/icon-512.png', '/apple-touch-icon.png'];
 const FONT_URLS  = [
